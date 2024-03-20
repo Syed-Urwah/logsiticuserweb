8211e274471ccce33b7722d72f074e1640cb3799
Okasha Khan <151372881+okashakhan66@users.noreply.github.com>
2024-03-20 12:39:03 +0500

    shadui

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function page() {
   return (
     <main className='flex min-h-screen flex-col'>
-      
+      <Button>Click me</Button>
     </main>
   )
 }
