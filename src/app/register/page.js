6c10cf61bb609a89aacba919ac74635836c878c7
Okasha Khan <151372881+okashakhan66@users.noreply.github.com>
2024-03-20 12:19:21 +0500

    header

---
+++ src/app/register/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image from "next/image";
+
+export default function page() {
+  return (
+    <main className='flex min-h-screen flex-col'>
+      
+    </main>
+  )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <div>
-      header
-    </div>
-  )
+    <header className="bg-indigo-500 h-12">
+      <div className="flex justify-around">
+        <div>
+          <a href="#">
+            <Image
+              src="/images/logo.png"
+              alt="Vercel Logo"
+              width={100}
+              height={30}
+              className="pt-2"
+              priority
+            />
+          </a>
+        </div>
+
+        <div>
+          <a href="#" className="flex mt-3">
+            <img
+              src="/images/location.png"
+              alt="Vercel Logo"
+              className="h-5 me-3"
+            />
+            <p
+              style={{
+                fontFamily: "Montserrat",
+                fontWeight: 500,
+                fontSize: 16,
+              }}
+            >
+              xyz road, Riyadh, Saudia Arabia
+            </p>
+          </a>
+        </div>
+
+        <div>
+          <a href="#" className="flex mt-3">
+            <img src="/images/phone.png" alt="phone" className="h-5 me-3" />
+            <p
+              style={{
+                fontFamily: "Montserrat",
+                fontWeight: 500,
+                fontSize: 16,
+              }}
+            >
+              0335-3915158
+            </p>
+          </a>
+        </div>
+
+        <div className="flex mt-3">
+          <a href="#">
+            <img src="/images/notifi.png" alt="phone" className="h-5 me-14" />
+          </a>
+          <a href="#">
+            <p
+              style={{
+                fontFamily: "Montserrat",
+                fontWeight: 600,
+                fontSize: 15,
+              }}
+            >
+              CALL US NOW
+            </p>
+          </a>
+        </div>
+      </div>
+
+      <nav className="flex"></nav>
+    </header>
+  );
 }
