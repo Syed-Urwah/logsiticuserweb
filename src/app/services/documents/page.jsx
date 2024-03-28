babc492738235790117c7cc39c80b537977061a9
khanmsalman <salmankhan775543@gmail.com>
2024-03-28 11:20:29 +0500

    udpate services page

---
+++ src/app/services/documents/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ServicesDocuments = () => {
+    return (
+        <div>
+            <div>
+                <h1 className="">
+                    Apaale: Your One-Stop Shop for Secure & Speedy Logistics Documents
+                </h1>
+                <p className="">
+                    In the broad realm of logistics, where products are moved between countries and continents, Documentation is essential to maintaining efficient operations. Leading logistics software company Apaale provides solutions that simplify this procedure, enabling companies to more easily handle their shipments.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ServicesDocuments
