01bfb0404f93daab92c6ffb5f079db23e603ff80
Sabahat-Sarwar <sabahatsarwar341@gmail.com>
2024-04-02 22:45:57 +0500

    book a ride Api integ error

--- src/app/book-a-ride/page.js
+++ src/app/book-a-ride/page.js
@@ -1,5 +1,8 @@
+"use client";
+
 import * as React from "react"
 import "../styles/custom.css";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button"
 import {
@@ -19,37 +22,119 @@ import {
 import CallUs from "@/components/CallUs"
 
 export default function page() {
+  const [bookaRide, setRide]= useState ({
+    user_id:"",
+    no_of_person: "",
+    vehicle_list: "",
+    contact_info: "",
+    total_km:"",
+    origin:"",
+    destination: "",
+    amount: "",
+  });
+  
+  const handleRide = async (e) => {
+    e.preventDefault();
+    console.log(bookaRide)
+
+    const url = process.env.NEXT_PUBLIC_SERVER_BASE_URL + '/api/customer/booking_rides/search_driver'
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                // "X-CSRF-TOKEN": csrfToken,
+                "Api-Token": "N5ORjSS300F4fcZ3eq69rLShvgwnjchQg7Vmt5N753Sy"
+            },
+            body: JSON.stringify(bookaRide)
+        });
+    
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+    
+
+    
+  }
   return (
     <div>
       <div className="2xl:px-[320px] xl:px-[120px] max-xl:px-[100px] my-7">
         <div className="mx-auto w-full sm:max-w-sm lg:max-w-full mb-5">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">BOOK A RIDE</h2>
         </div>
-        <form className="pt-3 px-2 text-gray-400">
+        <form onSubmit={handleRide} className="pt-3 px-2 text-gray-400">
 
           <Card className="w-full py-6 bg-gray-50 rounded-xl">
             <CardContent>
               <div className="grid w-full  items-center gap-4">
+              <div className="flex max-lg:flex-col md:justify-between">
+                  <div className="flex  items-center  max-sm:flex-col text-nowrap space-y-1.5">
+                    <Label htmlFor="persons" className='text-base sm:me-8'>User Id</Label>
+                    <Input id="user_id"  type="text" required className='lg:w-[250px] xl:w-[300px] max-lg:ms-7 sm:ms-7 max-sm:ms-2 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" 
+                     onChange={(e)=> {
+                      setRide((prev) => ({
+                        prev,
+                        user_id: e.target.value,
+                      }));
+                    }}/>
+                  </div>
+                </div>
 
                 <div className="flex max-lg:flex-col md:justify-between">
                   <div className="flex  items-center max-sm:flex-col  text-nowrap space-y-1.5">
                     <Label htmlFor="persons" className='text-base'>No of Persons</Label>
-                    <Input id="name" required className='lg:w-[250px] xl:w-[300px] sm:ms-4 border-[2px] rounded border-blue-300  ' placeholder="Name of your project" />
+                    <Input id="no_of_person"  type="text" required className='lg:w-[250px] xl:w-[300px] sm:ms-4 border-[2px] rounded border-blue-300  ' placeholder="Name of your project" 
+                    onChange={(e)=> {
+                      console.log(e.target.value)
+                      setRide((prev) => ({
+                        prev,
+                        no_of_person: e.target.value,
+                      }));
+                    }}
+                    />
                   </div>
                   <div className="flex items-center max-sm:flex-col text-nowrap space-y-1.5 max-lg:mt-4">
                     <Label htmlFor="persons" className='text-base'>Vehicle's List</Label>
-                    <Input id="name" required className='lg:w-[250px] xl:w-[300px] sm:ms-6 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" />
+                    <Input id="vehicle_list"  type="text" required className='lg:w-[250px] xl:w-[300px] sm:ms-6 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" 
+                      onChange={(e)=>{
+                        console.log(e.target.value)
+                        setRide((prev) => ({
+                            ...prev,
+                            vehicle_list: e.target.value,
+                          }));
+                      }} 
+                    />
                   </div>
                 </div>
 
                 <div className="flex max-lg:flex-col md:justify-between">
                   <div className="flex  items-center  max-sm:flex-col text-nowrap space-y-1.5">
                     <Label htmlFor="persons" className='text-base'>Contact Info</Label>
-                    <Input id="name" required className='lg:w-[250px] xl:w-[300px] max-lg:ms-7 sm:ms-7 max-sm:ms-2 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" />
+                    <Input id="contact_info"  type="text" required className='lg:w-[250px] xl:w-[300px] max-lg:ms-7 sm:ms-7 max-sm:ms-2 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" 
+                     onChange={(e)=> {
+                      console.log(e.target.value)
+                      setRide((prev) => ({
+                        prev,
+                        contact_info: e.target.value,
+                      }));
+                    }}
+                    />
                   </div>
                   <div className="flex items-center max-sm:flex-col text-nowrap space-y-1.5 max-lg:mt-4">
                     <Label htmlFor="persons" className='text-base'>Total Km</Label>
-                    <Input id="name" required className='lg:w-[250px] xl:w-[300px] sm:ms-14 max-sm:ms-3 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" />
+                    <Input id="total_km"  type="text" required className='lg:w-[250px] xl:w-[300px] sm:ms-14 max-sm:ms-3 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" 
+                     onChange={(e)=> {
+                      console.log(e.target.value)
+                      setRide((prev) => ({
+                        prev,
+                        total_km: e.target.value,
+                      }));
+                    }}
+                    />
                   </div>
                 </div>
 
@@ -62,7 +147,15 @@ export default function page() {
                     </svg>
                     </span>
                     <Label htmlFor="persons" className='text-base sm:me-6' >Origin</Label>
-                    <Input id="name" required className='  max-lg:ms-7 sm:ms-7 max-sm:ms-2 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" />
+                    <Input id="origin"  type="text" required className='  max-lg:ms-7 sm:ms-7 max-sm:ms-2 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" 
+                     onChange={(e)=> {
+                      console.log(e.target.value)
+                      setRide((prev) => ({
+                        prev,
+                       origin: e.target.value,
+                      }));
+                    }}
+                    />
                   </div>
                   <div className="flex  items-center mt-4  max-sm:flex-col text-nowrap space-y-1.5">
                     <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -71,7 +164,15 @@ export default function page() {
                     </svg>
                     </span>
                     <Label htmlFor="persons" className='text-base'>Destination</Label>
-                    <Input id="name" required className=' max-lg:ms-7 sm:ms-3  max-sm:ms-2 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" />
+                    <Input id="destination"  type="text" required className=' max-lg:ms-7 sm:ms-3  max-sm:ms-2 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" 
+                     onChange={(e)=> {
+                      console.log(e.target.value)
+                      setRide((prev) => ({
+                        prev,
+                        destination: e.target.value,
+                      }));
+                    }}
+                    />
 
                   </div>
 
@@ -80,7 +181,14 @@ export default function page() {
                 <div className="flex max-lg:flex-col md:justify-between">
                   <div className="flex  items-center  max-sm:flex-col text-nowrap space-y-1.5">
                     <Label htmlFor="persons" className='text-base sm:me-8'>Amount</Label>
-                    <Input id="name" required className='lg:w-[250px] xl:w-[300px] max-lg:ms-7 sm:ms-7 max-sm:ms-2 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" />
+                    <Input id="amount"  type="text" required className='lg:w-[250px] xl:w-[300px] max-lg:ms-7 sm:ms-7 max-sm:ms-2 border-[2px] rounded border-blue-300 ' placeholder="Name of your project" 
+                     onChange={(e)=> {
+                      console.log(e.target.value)
+                      setRide((prev) => ({
+                        prev,
+                        amount: e.target.value,
+                      }));
+                    }}/>
                   </div>
                 </div>
 
@@ -89,7 +197,7 @@ export default function page() {
           </Card>
 
           <div className="flex justify-center my-7">
-            <button className="py-3 px-4 rounded-xl bg-[#6C63FF] text-white">
+            <button type="submit" className="py-3 px-4 rounded-xl bg-[#6C63FF] text-white">
               Confirm Ride
             </button>
           </div>
